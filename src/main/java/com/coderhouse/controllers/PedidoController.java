ed00045d754445dfd6150b56eacc80d397350848
Fernando Zelaya <fernando.zelaya@claro.com.ar>
2025-01-13 18:20:31 -0300

    Avance del proyecto

--- src/main/java/com/coderhouse/controllers/PedidoController.java
+++ src/main/java/com/coderhouse/controllers/PedidoController.java
@@ -1,8 +1,10 @@
 package com.coderhouse.controllers;
 
+
 import com.coderhouse.models.Pedido;
 import com.coderhouse.services.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +18,8 @@ public class PedidoController {
     @PostMapping
     public ResponseEntity<Pedido> crearPedido(@RequestBody Pedido pedido) {
         Pedido nuevoPedido = pedidoService.crearPedido(pedido);
-        System.out.println("nuevoPedido: "+nuevoPedido.toString());
-        return ResponseEntity.status(201).body(nuevoPedido);
+        ResponseEntity.status(HttpStatus.CREATED).body(nuevoPedido);
+        //ResponseEntity.status(201).body(nuevoPedido);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoPedido);
     }
 }
